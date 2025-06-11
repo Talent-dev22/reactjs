262939f090c1ae2c351de19f58daa6c02d8ce864
kushal <kushal@gmail.com>
2025-06-11 13:44:35 +0000

    updated ecr details

--- Jenkinsfile-react.groovy
+++ Jenkinsfile-react.groovy
@@ -10,18 +10,18 @@ pipeline {
            stage('docker upload'){
             steps{
                 sh """
-                  docker tag reactjsapp:v${BUILD_NUMBER} malleshdevops/devops21:reactjsapp-v${BUILD_NUMBER}
-                  docker push malleshdevops/devops21:reactjsapp-v${BUILD_NUMBER}
+                  docker tag reactjsapp:v${BUILD_NUMBER} malleshdevops/dev22:reactjsapp-v${BUILD_NUMBER}
+                  docker push malleshdevops/dev22:reactjsapp-v${BUILD_NUMBER}
                 """
             }
            }
            stage('push ECR'){
             steps {
                 sh '''
-                aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 891612549251.dkr.ecr.us-west-2.amazonaws.com
+                aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 759449706669.dkr.ecr.us-west-2.amazonaws.com
 
-                docker tag reactjsapp:v${BUILD_NUMBER} 891612549251.dkr.ecr.us-west-2.amazonaws.com/talent/reactjsapp:v${BUILD_NUMBER}
-                docker push 891612549251.dkr.ecr.us-west-2.amazonaws.com/talent/reactjsapp:v${BUILD_NUMBER}
+                docker tag reactjsapp:v${BUILD_NUMBER} 759449706669.dkr.ecr.us-west-2.amazonaws.com/reactjs/react22:v${BUILD_NUMBER}
+                docker push 759449706669.dkr.ecr.us-west-2.amazonaws.com/reactjs/react22:v${BUILD_NUMBER}
        '''
             }
           }
